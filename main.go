f9feb69e3978502684a59c43f030741603ee0bb0
zachthomas823 <zachthomas823@gmail.com>
2020-01-07 15:17:52 -0600

    Added the ability to specify absolute answers.
    The program will only answer yes or no if the user inputs "yes" when prompted.

--- main.go
+++ main.go
@@ -1,30 +1,49 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"math/rand"
+	"os"
+	"strings"
 	"time"
 )
 
 func main() {
-	var t int64
-	t = int64(time.Now().Second())
-	rand.Seed(t)
-	n := rand.Intn(6)
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Println("Are you sure you want to know? [y/n]")
+	fmt.Println()
+	fmt.Print("-->")
+	in, _ := reader.ReadString('\n')
+
+	var absolute bool
+	if strings.Replace(in, "\n", "", 1) == "y" {
+		absolute = true
+	} else if strings.Replace(in, "\n", "", 1) == "n" {
+		absolute = false
+	} else {
+		absolute = false
+		fmt.Println()
+		fmt.Println("I don't know what that means, so I guess you dont care that much.")
+		fmt.Println()
+	}
+
 	var responses [6]string
 	responses[0] = "Yes"
 	responses[1] = "No"
 	responses[2] = "Maybe"
 	responses[3] = "Ask me after another beer"
 	responses[4] = "Terrible idea ... do it"
-	responses[5] = "Hahahahahahahaha"
-	fmt.Println(responses[n])
+	responses[5] = "Hahahahahahahaha ... no"
 
-	// if n == 0 {
-	// 	println("No")
-	// } else if n == 1 {
-	// 	println("Yes")
-	// } else if n == 2 {
-	// 	println("Maybe")
-	// }
+	numResponses := len(responses)
+	if absolute {
+		numResponses = 2
+	}
+	var t int64
+	t = int64(time.Now().Second())
+	rand.Seed(t)
+	n := rand.Intn(numResponses)
+	fmt.Println()
+	fmt.Println(responses[n])
 }
