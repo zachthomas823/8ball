5f0d362ac0a10fcd86050bc17fe1d3d81df5047c
zachthomas823 <zachthomas823@gmail.com>
2020-01-07 13:54:10 -0600

    Added 3 more responses, replaced if else statements with accessing array

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"math/rand"
 	"time"
 )
@@ -9,12 +10,21 @@ func main() {
 	var t int64
 	t = int64(time.Now().Second())
 	rand.Seed(t)
-	n := rand.Intn(3)
-	if n == 0 {
-		println("No")
-	} else if n == 1 {
-		println("Yes")
-	} else if n == 2 {
-		println("Maybe")
-	}
+	n := rand.Intn(6)
+	var responses [6]string
+	responses[0] = "Yes"
+	responses[1] = "No"
+	responses[2] = "Maybe"
+	responses[3] = "Ask me after another beer"
+	responses[4] = "Terrible idea ... do it"
+	responses[5] = "Hahahahahahahaha"
+	fmt.Println(responses[n])
+
+	// if n == 0 {
+	// 	println("No")
+	// } else if n == 1 {
+	// 	println("Yes")
+	// } else if n == 2 {
+	// 	println("Maybe")
+	// }
 }
